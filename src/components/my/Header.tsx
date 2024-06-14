9311337e6564098733ca3bb05d189c0ee3b60135
vinceflores <vincegabriel.flores@gmail.com>
2024-06-14 00:44:56 -0400

    fixing responsiveness

--- src/components/my/Header.tsx
+++ src/components/my/Header.tsx
@@ -2,7 +2,7 @@ import { FlipWords } from "../acernity/flip-words";
 function Header() {
   const words = ["Engineer", "Developer"]
   return (
-    <div className=" sticky bottom-0  hidden sm:flex flex-col mx-5 mb-5  text-left items-start justify-start">
+    <div className=" sticky bottom-auto lg:absolute lg:bottom-0  hidden sm:flex flex-col mx-5 mb-5  text-left items-start justify-start">
       <h1 className="font-mono  text-sm md:text-3xl">Vince Flores</h1>
       <p className="text-xs">
         Software <FlipWords words={words} />
